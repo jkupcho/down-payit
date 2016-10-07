620a2e8a9f7a70441350b5eabde22bb2670565ae
jkupcho <jonathan.kupcho@gmail.com>
2016-10-06 20:15:46 -0500

    typo

--- src/reducers/interest.js
+++ src/reducers/interest.js
@@ -1,6 +1,6 @@
 /***
  * Separating interest into its own slice of state due to the fact that
- * it's the only piece of state that relies on ansynchronous data. If
+ * it's the only piece of state that relies on asynchronous data. If
  * it was kept within the form state, it'd muddy things up with a lot of
  * promise logic.
  */
